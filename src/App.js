6cc095cdfef4b3bf4d149e5016d2700be803006d
Gavyn Caldwell <gavman@Macbooks-MacBook-Air.local>
2017-11-29 17:00:53 -0700

    made everything look better on widescreens, changed media query mixin

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react'
 import { HashRouter as Router, Route } from 'react-router-dom'
 
 import './styles.scss'
+import Footer from './components/footer'
 import Header from './components/header'
 import HomePage from './pages/home'
 import HireMePage from './pages/hire-me'
 import MessageReceived from './pages/message-received'
 
 export default class App extends Component {
-  render () {
+  render() {
     return (
       <Router>
         <div className='o-page'>
@@ -16,6 +17,7 @@ export default class App extends Component {
           <Route exact path='/' component={HomePage} />
           <Route exact path='/hire' component={HireMePage} />
           <Route exact path='/message-received' component={MessageReceived} />
+          {/* <Footer /> */}
         </div>
       </Router>
     )
